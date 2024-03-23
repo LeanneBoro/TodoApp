d2c88cfc0bcaf30b74ef9fff8d6ce5a3c8b0d8e4
LeanneBoro <leanneboro@gmail.com>
2024-03-23 16:50:13 +0200

    Add service file

--- services/todo-service.js
+++ services/todo-service.js
@@ -1,11 +1,33 @@
+import { storageService } from "./async-storage.service.js"
 
 
 export const todoService = {
-    query
+    query,
+    remove,
+    save
 }
 
 const TODO_KEY = 'todoDB'
 
-function query() {
+function query(filterBy) {
 
+    return storageService.query(TODO_KEY)
+        .then(todos => {
+            return todos
+        })
+
+}
+
+
+function remove(todoId) {
+    return storageService.remove(TODO_KEY,todoId)
+}
+
+
+function save(todo) {
+    if (todo.id) {
+        return storageService.put(TODO_KEY, todo)
+    } else {
+        return storageService.post(TODO_KEY, todo)
+    }
 }
